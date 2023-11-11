91ecb2a22ac1851dd26ac98da2f93014b7f70ff0
dntuanduy-124 <143875347+dntuanduy-124@users.noreply.github.com>
2023-11-11 16:56:44 +0700

    Add project files.

---
+++ SharkerSort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharkerSort
+{
+    internal class Program
+    {
+        static void TestShakerSort()
+        {
+            Console.WriteLine("Shaker Sort");
+            Console.Write("Nhap cac phan tu cua mang: ");
+            var parts = Console.ReadLine().Split(new[] { "", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+            var array = new int[parts.Length];
+            for (int i = 0; i < parts.Length; i++)
+            {
+                array[i] = int.Parse(parts[i]);
+            }
+
+            Console.WriteLine("mang sau khi sap xep: {0}", string.Join(",", (array)));
+        }
+        
+
+        static void Main(string[] args)
+        {
+            TestShakerSort();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SharkerSort/Shaker_Sort.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharkerSort
+{
+    internal class Shaker_Sort_Array
+    {
+        // sắp xếp ngẫu nhiên
+        public int[] MySharkerSort(int[] array)
+        {
+            for (var i = 0; i < array.Length / 2; i++)
+            {
+                var swapFlag = false;
+                // xếp từ trái sang phải
+                for (var j = i; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                        swapFlag = true;
+                    }
+                }
+
+                // xếp từ phải sang trái
+                for (var j = array.Length - 2 - i; j > i; j--)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        Swap(ref array[j - 1], ref array[j]);
+                        swapFlag = true;
+                    }
+                }
+
+                // nếu không có trao đổi thì thoát
+                if (!swapFlag)
+                {
+                    break;
+                }
+
+            }
+            return array;
+        }
+
+        public void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
+
